0ece4ea3284de9bed71acf97a3fdf9eff591b4e3
ablog666 <54060082+ablog666@users.noreply.github.com>
2020-05-06 23:25:42 +0300

    123

---
+++ pages/content1.php
@@ -0,0 +1,93 @@
+
+<div class="wrapper row2 bgded" style="background-image:url('../images/demo/backgrounds/01.png');">
+  <div class="overlay">
+    <div id="breadcrumb" class="clear"> 
+      <!-- ################################################################################################ -->
+      <ul>
+        <li><a href="/gerd/index.php">Home</a></li>
+        <li><a href="#">Work</a></li>
+      </ul>
+      <!-- ################################################################################################ -->
+    </div>
+  </div>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row3">
+  <main class="container clear"> 
+    <!-- main body -->
+    <!-- ################################################################################################ -->
+    <div class="sidebar one_quarter first"> 
+      <!-- ################################################################################################ -->
+      <h6>Список</h6>
+      <nav id="mainav" class="fl_right">
+        <ul>
+          
+              
+          <li><a href="#">Апрель</a>
+            <ul>
+              <li class="active"><a class="drop" href="#">1-15</a>
+              <ul>
+                  <li><a href="1.php">1 среда</a></li>
+                  <li><a href="2.php">2 четверг</a></li>
+                  <li><a href="3.php">3 пятница</a></li>
+                  <li><a href="6.php">6 понедельник</a></li>
+                  <li><a href="7.php">7 вторник</a></li>
+                  <li><a href="8.php">8 среда</a></li>
+                  <li><a href="9.php">9 четверг</a></li>
+                  <li><a href="10.php">10 пятница</a></li>
+                  <li><a href="13.php">13 понедельник</a></li>
+                  <li><a href="14.php">14 вторник</a></li>
+                  <li><a href="15.php">15 среда</a></li>
+                </ul>
+              </li>
+              <li class="active"><a class="drop" href="#">15-30</a>
+                <ul>
+                  <li><a href="16.php">16 четверг</a></li>
+                  <li><a href="17.php">17 пятница</a></li>
+                  <li><a href="20.php">20 понедельник</a></li>
+                  <li><a href="21.php">21 вторник</a></li>
+                  <li><a href="22.php">22 среда</a></li>
+                  <li><a href="23.php">23 четверг</a></li>
+                  <li><a href="24.php">24 пятница</a></li>
+                  <li><a href="27.php">27 понедельник</a></li>
+                  <li><a href="28.php">28 вторник</a></li>
+                  <li><a href="29.php">29 среда</a></li>
+                  <li><a href="30.php">30 четверг</a></li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          <li><a href="#">Май</a>
+            <ul>
+              <li class="active"><a class="drop" href="#">1-15</a>
+              <ul>
+                  <li><a href="1.php">1 пятница</a></li>
+                  <li><a href="4.php">4 понедельник</a></li>
+                  <li><a href="5.php">5 вторник</a></li>
+                  <li><a href="6.php">6 среда</a></li>
+                  <li><a href="7.php">7 четверг</a></li>
+                  <li><a href="8.php">8 пятница</a></li>
+                  <li><a href="11.php">11 понедельник</a></li>
+                  <li><a href="12.php">12 вторник</a></li>
+                  <li><a href="13.php">13 среда</a></li>
+                  <li><a href="14.php">14 четверг</a></li>
+                  <li><a href="15.php">15 пятница</a></li>
+                </ul>
+              </li>
+              <li class="active"><a class="drop" href="#">15-25</a>
+                <ul>
+                  <li><a href="18.php">18 понедельник</a></li>
+                  <li><a href="19.php">19 вторник</a></li>
+                  <li><a href="20.php">20 среда</a></li>
+                  <li><a href="21.php">21 четверг</a></li>
+                  <li><a href="22.php">22 пятница</a></li>
+                  <li><a href="25.php">25 понедельник</a></li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+      </ul>
+      </nav>
+</div>  
---
+++ pages/header1.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<!--
+Template Name: Viral
+Author: <a href="https://www.os-templates.com/">OS Templates</a>
+Author URI: https://www.os-templates.com/
+Licence: Free to use under our free template licence terms
+Licence URI: https://www.os-templates.com/template-terms
+-->
+<html>
+<head>
+<title>Дистанционное обучение</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href="/layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body id="top">
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row0">
+  <div id="topbar" class="clear"> 
+    <!-- ################################################################################################ -->
+    <div class="fl_left">
+      <ul class="nospace inline">
+        <li><i class="fa fa-envelope-o"></i> vv_s1@mail.ru</li>
+        <li><i class="fa fa-envelope-o"></i> VK: <a href="https://vk.com/mar.marrries">@mar.marrries</a></li>
+      </ul>
+    </div>
+    <div class="fl_right">
+      <ul class="faico clear">
+        <li><a class="faicon-facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+        <li><a class="faicon-pinterest" href="#"><i class="fa fa-pinterest"></i></a></li>
+        <li><a class="faicon-twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+        <li><a class="faicon-dribble" href="#"><i class="fa fa-dribbble"></i></a></li>
+        <li><a class="faicon-linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
+        <li><a class="faicon-google-plus" href="#"><i class="fa fa-google-plus"></i></a></li>
+        <li><a class="faicon-rss" href="#"><i class="fa fa-rss"></i></a></li>
+      </ul>
+    </div>
+    <!-- ################################################################################################ -->
+  </div>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row1">
+  <header id="header" class="clear"> 
+    <!-- ################################################################################################ -->
+    <div id="logo" class="fl_left">
+      <h1><a href="index.php">Дистанционное обучение</a></h1>
+    </div>
+    <nav id="mainav" class="fl_right">
+      <ul class="clear">
+        <li class="active"><a href="index.php">Домашняя страница</a></li>
+        <li><a class="drop" href="#">Числа</a>
+          <ul>
+            <li><a href="pages/work.html">Числа</a></li><!--- сменить хреф-->
+          </ul>
+        </li>
+      </ul>
+    </nav>
+    <!-- ################################################################################################ -->
+  </header>
